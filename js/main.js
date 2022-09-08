8542bfbb0beb6bb6e7db2e28de0f7f91fa8b3ca9
Alexandr Ilyasov <velmoren@yahoo.com>
2022-09-08 21:11:37 +0300

    dual slider

--- js/main.js
+++ js/main.js
@@ -1,32 +1,32 @@
 const rangeRunners = () => {
-  const runner = document.getElementById("customRange1");
-  const runner2 = document.getElementById("customRange2");
-  const blockLeft = document.getElementById("range-span1");
-  const blockRight = document.getElementById("range-span2");
+  // const runner = document.getElementById("customRange1");
+  // const runner2 = document.getElementById("customRange2");
+  // const blockLeft = document.getElementById("range-span1");
+  // const blockRight = document.getElementById("range-span2");
 
-  runner.addEventListener("input", (e) => {
-    if (+runner.value >= +runner2.value && +runner.value <= 99) {
-      runner2.value = +runner.value + 1;
-    } else if (+runner.value === 100) {
-      runner.value = 99
-      runner2.value = 100;
-    }
+  // runner.addEventListener("input", (e) => {
+  //   if (+runner.value >= +runner2.value && +runner.value <= 99) {
+  //     runner2.value = +runner.value + 1;
+  //   } else if (+runner.value === 100) {
+  //     runner.value = 99
+  //     runner2.value = 100;
+  //   }
 
-    blockLeft.textContent = runner.value;
-    blockRight.textContent = runner2.value;
-  });
+  //   blockLeft.textContent = runner.value;
+  //   blockRight.textContent = runner2.value;
+  // });
 
-  runner2.addEventListener("input", (e) => {
-    if (+runner2.value <= +runner.value && runner2.value >= 1) {
-      runner.value = +runner2.value - 1;
-    } else if (e.target.value == 0) {
-      runner2.value = 1;
-      runner.value = 0;
-    }
+  // runner2.addEventListener("input", (e) => {
+  //   if (+runner2.value <= +runner.value && runner2.value >= 1) {
+  //     runner.value = +runner2.value - 1;
+  //   } else if (e.target.value == 0) {
+  //     runner2.value = 1;
+  //     runner.value = 0;
+  //   }
 
-    blockLeft.textContent = runner.value;
-    blockRight.textContent = runner2.value;
-  });
+  //   blockLeft.textContent = runner.value;
+  //   blockRight.textContent = runner2.value;
+  // });
 };
 
 rangeRunners();
